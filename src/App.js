851d1f1cc8c6d188012e7d164515e9929254f3a6
Lucas <lucas@Lucass-MacBook-Pro.local>
2020-03-05 17:12:46 -0300

    analitycs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     ReactGA.initialize('G-3M22SELN4E');
-    ReactGA.pageview(window.location.pathname);
+    ReactGA.pageview(window.location.pathname + window.location.search);
 
   }
 
